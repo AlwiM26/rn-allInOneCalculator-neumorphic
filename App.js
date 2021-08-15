43483543e9b5a87ccbfd26a910f9d12a92e443ed
AlwiM26 <alwimhd01@gmail.com>
2021-08-15 18:56:01 +0700

    add container for number and operator, added operator button

--- App.js
+++ App.js
@@ -1,21 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 
-export default function App() {
+const App = () => {
+
+  const [n, setN] = useState('69 * 420');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      {/* Container for the number input */}
+      <View style={styles.numContainer}>
+        <Text style={styles.numText}>{n}</Text>
+      </View>      
+      
+      {/* Container for all the operator button */}
+      <View style={styles.operatorContainer}>
+
+        <View style={styles.column}>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.column}>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.column}>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.column}>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnOperator}>
+            <Text style={styles.textOperator}>AC</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  numContainer: {
+    flex: 1,
+    backgroundColor: 'pink',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  numText: {
+    fontSize: 40,    
+    padding: 15
+  },
+  operatorContainer: {
+    flex: 1,
+    backgroundColor: 'green',
+    flexDirection: 'row',
+  },
+  column: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  btnOperator: {
+    flex: 1,
+    backgroundColor: 'blue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    margin: 10,
+  },
+  textOperator: {
+    fontSize: 34,
   },
 });
+
+export default App;
