6357eaa42710d0bc3fc4f20535357559f8a0e2a0
AlwiM26 <AlwiMhd01@gmail.com>
2021-08-17 20:12:54 +0700

    fix all the operator function

--- App.js
+++ App.js
@@ -1,19 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 
 const App = () => {
 
-  const [n, setN] = useState('');
+  const [n, setN] = useState('0');
+  const [havePoint, setHavePoint] = useState(false);
   const op = [
-    [ {text: 'AC'}, {text: 'x'}, {text: '%'}, {text: ' / '} ],
-    [ {text: '7'}, {text: '8'}, {text: '9'}, {text: ' * '} ],
-    [ {text: '4'}, {text: '5'}, {text: '6'}, {text: ' - '} ],
-    [ {text: '1'}, {text: '2'}, {text: '3'}, {text: ' + '} ],
-    [ {text: '+/-'}, {text: '0'}, {text: '.'}, {text: '='} ],
+    [ {text: 'AC', type: 'clear'}, {text:  <Ionicons name="md-backspace-outline" size={25} color="#F85E18" />, type: 'backspace'}, {text: '%', type: 'percent'}, {text: '/', type: 'operator'} ],
+    [ {text: '7', type: 'number'}, {text: '8', type: 'number'}, {text: '9', type: 'number'}, {text: '*', type: 'operator'} ],
+    [ {text: '4', type: 'number'}, {text: '5', type: 'number'}, {text: '6', type: 'number'}, {text: '-', type: 'operator'} ],
+    [ {text: '1', type: 'number'}, {text: '2', type: 'number'}, {text: '3', type: 'number'}, {text: '+', type: 'operator'} ],
+    [ {text: '0', type: 'number'}, {text: '.', type: 'point'}, {text: '=', type: 'equal'} ],
   ];  
 
-  const handleOperator = (operator) => {
-    setN(n + operator);
+  const handleOperator = (type, val) => {
+    switch (type) {
+      case 'clear': 
+        setHavePoint(false);
+        setN('0');
+        break;
+      case 'backspace': 
+        return handleBackspace();
+      case 'percent': 
+        n !== '0' ? setN(`= ${eval(n) / 100}`) : setN('0');
+        break;
+      case 'equal':
+        setHavePoint(false);
+        setN(`= ${eval(n)}`);
+        break;
+      case 'posneg': 
+        setN(`${eval(n * -1)}`);
+        break;
+      case 'point':
+        if (!havePoint) {
+          setN(n + val);
+          setHavePoint(true);
+        }
+        break;
+      case 'operator': 
+      setHavePoint(false);
+        if (val !== n[n.length - 1]) {
+          setN(n + val);
+        } else {
+          setN(`${n}(${val}`);
+        }
+        break;
+      case 'number':
+        if (n === '0') { 
+          setN(val);
+        } else {
+          setN(n + val);
+        }
+        break;
+    }
+  }
+
+  const handleBackspace = () => {
+    setHavePoint(false);
+    if ( n[0] === '=' || n.length <= 1) {
+      setN('0');
+    } else {
+      setN(n.substring(0, n.length - 1));
+    }
   }
 
   return (
@@ -22,6 +71,8 @@ const App = () => {
       <View style={styles.numContainer}>
         <Text style={styles.numText}>{n}</Text>
       </View>      
+
+      <View style={{borderBottomColor: '#dedee0', borderBottomWidth: 1, marginHorizontal: 10}} />
       
       {/* Container for all the operator button */}
       <View style={styles.operatorContainer}>
@@ -31,8 +82,8 @@ const App = () => {
           <View style={styles.row} key={id}>
             {item.map((operator, id) => {
               return (          
-                <TouchableOpacity style={styles.btnOperator} onPress={() => handleOperator(operator.text)} key={id}>
-                  <Text style={styles.textOperator}>{operator.text}</Text>
+                <TouchableOpacity style={operator.type === 'equal' ? styles.btnEqual : styles.btnOperator} onPress={() => handleOperator(operator.type, operator.text)} key={id}>
+                  <Text style={{fontSize: 25, color: operator.type === 'number' ? 'black' : '#F85E18'}}>{operator.text}</Text>
                 </TouchableOpacity>
               )
             })}
@@ -51,7 +102,6 @@ const styles = StyleSheet.create({
   },
   numContainer: {
     flex: 1,
-    backgroundColor: 'pink',
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
   },
@@ -61,8 +111,9 @@ const styles = StyleSheet.create({
   },
   operatorContainer: {
     flex: 1,
-    backgroundColor: 'green',
     flexDirection: 'column',
+    paddingVertical: 5,
+    paddingHorizontal: 10
   },
   row: {
     flex: 1,
@@ -71,13 +122,21 @@ const styles = StyleSheet.create({
   },
   btnOperator: {
     flex: 1,
-    // backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'space-around',
     margin: 10,
+    borderRadius: 100,
+    borderColor: '#DEDEE0',
+    borderWidth: 1,
   },
-  textOperator: {
-    fontSize: 34,
+  btnEqual: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    margin: 10,
+    borderRadius: 100,
+    borderColor: '#DEDEE0',
+    borderWidth: 1,
   },
 });
 
